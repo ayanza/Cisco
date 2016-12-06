58500ea928b89d1ab19d60a754106329658ec7ec
CISCO <angelayanza@gmail.com>
2016-12-05 21:04:23 -0500

    test

---
+++ practica2.py
@@ -0,0 +1,43 @@
+print (" --------- PRÁCTICA 2 ------- ")
+print (" --------- TIPOS DE DATOS ------- ")
+# Uso de la función type  para tipos de datos
+"""
+print (type (10))
+print (type (5.5))
+print (type (1+5j))
+"""
+# Comentar bloques con tres comillas dobles antes y después
+
+
+print (type (10))
+print (type (5.5))
+print (type (1+5j))
+
+#Operadores
+print()
+print ("Manejo de operadores matemáticos")
+print ("================================")
+#endl="\n\n"
+print(2**10)
+print (2+4)
+print (1+5j)
+
+print()
+print ("Manejo de cadena de caracteres")
+print ("==============================")
+print ('Ángela')
+print ("""
+Línea 1
+Línea 2
+Línea 3
+""")
+print (type ("esto es una cadena"))
+#Concatenando cadenas
+print ("Concatenando cadenas: ")
+print ("cadena 1 " + "cadena 2")
+#Multiplicación de una cadena por un escalar
+print ("cadena 1 "*3)
+
+#Valores booleanos
+print (type(True))
+print (type(False))
---
+++ practica3.py
@@ -0,0 +1,18 @@
+#Variables
+print ("VARIABLES")
+
+a = 5
+print (a)
+print (type(a))
+
+a = "cinco"
+print (a)
+print (type(a))
+
+nombre = "Ángela"
+print (nombre) #las mayus/minus son diferentes
+
+año ="2016"
+print (año)
+
+#Constantes
